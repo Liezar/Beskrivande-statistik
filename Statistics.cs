332d5c45a0fd9900a997f1c678b731a134745fc6
Liezar <Edvin.salminen@gmail.com>
2022-10-17 15:09:11 +0200

    Lagt till Statistics klassen

---
+++ Statistics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beskrivande_statistik
+{
+    public static class Statistics
+    {
+    }
+}
