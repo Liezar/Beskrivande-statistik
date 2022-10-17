fc102112ea4c953ce9bcec84bd74f1ac74b011b6
Liezar <Edvin.salminen@gmail.com>
2022-10-17 13:37:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Beskrivande_statistik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
