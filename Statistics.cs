00aca3379daa9f64494a2372056894e2e628ebb0
Adam Andersson <adam.andersson14@learnet.se>
2022-10-18 11:06:01 +0200

    Gjort metoderna

--- Statistics.cs
+++ Statistics.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Beskrivande_statistik
 {
     public static class Statistics
     {
+        public static int GetMaximum()
+        {
+
+            int[] inData = Inputs.ImportJSON();
+            return inData.Max();
+
+        }
+
+        public static int GetMinimum()
+        {
+
+            int[] inData = Inputs.ImportJSON();
+            return inData.Min();
+        }
+
+        public static int GetRange()
+        {
+            int range = GetMaximum() - GetMinimum();
+            return range;
+        }
     }
+
 }
