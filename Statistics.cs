bb56a082f63c59a8220ac95bc3df8dfa4f4089e7
Liezar <Edvin.salminen@gmail.com>
2022-10-18 13:06:49 +0200

    Kommenterat Mode metoden

--- Statistics.cs
+++ Statistics.cs
@@ -1,43 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Serialization;
-using System.Runtime;
+﻿using System.Text;
 
 namespace Beskrivande_statistik
 {
     public static class Statistics
     {
+        //En metod för att ta de mest förekommande talen i json-filen och returnera det till en int[]
         public static int[] Mode()
         {
+            //Hämtar datan från json-filen och lagrar i variablen
             var jsonNumbers = Inputs.ImportJSON();
+
+            //En list som lagrar typvärden
             List<int> KeyList = new List<int>();
-            
+
+            //Kollar alla tal och räknar hur många gånger talen förekommer
             var counts = jsonNumbers.GroupBy(i => i).Select(grp => new { grp.Key, Count = grp.Count() });
+
+            //Lägger till alla tal som förkommer flest gånger i itemsMax
             var itemsMax = counts.Where(x => x.Count == counts.Max(y => y.Count));
 
+            //Sorterar i vilken ordning som items läggs till i Keylist
             foreach (var item in itemsMax.OrderBy(x => x.Key))
             {
                 KeyList.Add(item.Key);
             }
+
+            //Konverterar listan till en array och returnerar detta
             return KeyList.ToArray();
-        }     
+        }
 
         public static double GetStandardDeviation()
         {
-           //https://learn.microsoft.com/en-us/dotnet/api/system.math.round?view=net-7.0
-           int[] nums = Inputs.ImportJSON();
-           //hittar medelvärde
-           double deviation = nums.Average();
-           //formel för standardavikelse, avrundar resultat till en decimal
-           double avarageDeviation = nums.Select(val => (val - deviation) * (val - deviation)).Sum();
-           double standardDeviation = Math.Round(Math.Sqrt(avarageDeviation / nums.Length),1);
-            
-           
-           return standardDeviation;
+            //https://learn.microsoft.com/en-us/dotnet/api/system.math.round?view=net-7.0
+            int[] nums = Inputs.ImportJSON();
+            //hittar medelvärde
+            double deviation = nums.Average();
+            //formel för standardavikelse, avrundar resultat till en decimal
+            double avarageDeviation = nums.Select(val => (val - deviation) * (val - deviation)).Sum();
+            double standardDeviation = Math.Round(Math.Sqrt(avarageDeviation / nums.Length), 1);
+
+            return standardDeviation;
         }
 
         public static int GetMedian()
@@ -57,9 +59,57 @@ namespace Beskrivande_statistik
             {
                 // ojämnt antal
                 return sortednums[itemIndex];
-                
             }
-                
+        }
+
+        public static int GetMaximum()
+        {
+            int[] inData = Inputs.ImportJSON();
+            return inData.Max();
+        }
+
+        public static int GetMinimum()
+        {
+            int[] inData = Inputs.ImportJSON();
+            return inData.Min();
+        }
+
+        public static int GetRange()
+        {
+            int range = GetMaximum() - GetMinimum();
+            return range;
+        }
+        public static dynamic DescriptiveStatistics()
+        {
+            dynamic allData = new Dictionary<string, object>() { };
+
+            StringBuilder modeResault = new StringBuilder();
+            foreach (var item in Mode())
+            {
+                modeResault.Append($"{item}, ");
+            }
+
+            allData.Add("Maximum", GetMaximum());
+            allData.Add("Minimum", GetMinimum());
+            allData.Add("Medelvärde", GetMean());
+            allData.Add("Median", GetMedian());
+            allData.Add("Typvärde", modeResault);
+            allData.Add("Variationsbredd", GetRange());
+            allData.Add("Standardavvikelse", GetStandardDeviation());
+
+            return allData;
+        }
+
+        public static double GetMean()
+        {
+            var inData = Inputs.ImportJSON();
+            double sum = 0;
+            foreach (int x in inData)
+            {
+                sum += x;
+            }
+            double rounded = Math.Round(sum / inData.Length, 1);
+            return rounded;
         }
     }
 }
