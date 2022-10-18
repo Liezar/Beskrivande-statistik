b06c9bd8d7d1e07a1ee14b489bc80643f63fe0b7
Adam Andersson <adam.andersson14@learnet.se>
2022-10-18 11:04:26 +0200

    Gjort klart mina metoder.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
 ﻿using Newtonsoft.Json;
+using System.Linq;
 namespace Beskrivande_statistik
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine(Statistics.GetMaximum());
+            Console.WriteLine(Statistics.GetMinimum());
+            Console.WriteLine(Statistics.GetRange());
+
         }
     }
 }
